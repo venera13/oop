c3d3547f1ebe4edacd21ceb090be8a82311acd56
venera13 <lobanova_venera@mail.ru>
2020-02-20 21:28:02 +0300

    Поправила tests.bat

--- ConsoleApplication1/tests.bat
+++ ConsoleApplication1/tests.bat
@@ -6,27 +6,32 @@ if %MyProgram%=="" (
 	exit /B 1
 )
 
-REM  Copy empty file
-%MyProgram% empty.txt "%TEMP%\output.txt" || goto err
-fc empty.txt "%TEMP%\output.txt" > nul || goto err
+REM  Copy file
+%MyProgram% input.txt "%TEMP%\tests\output.txt" || goto err
+fc input.txt "%TEMP%\output.txt" > nul || goto err
 echo test 1 passed
 
-REM  Copy non empty file
-%MyProgram% notEmpty.txt "%TEMP%\output.txt" || goto err
-fc notEmpty.txt "%TEMP%\output.txt"> nul || goto err
+REM  Copy empty file
+%MyProgram% empty.txt "%TEMP%\tests\empty.txt" || goto err
+fc empty.txt "%TEMP%\empty.txt" > nul || goto err
 echo test 2 passed
 
-REM  Copy missing file should fail
-%MyProgram% none.txt "%TEMP%\output.txt" > nul && goto err
+REM  Copy non empty file
+%MyProgram% notEmpty.txt "%TEMP%\tests\NonEmptyOutput.txt" || goto err
+fc notEmpty.txt "%TEMP%\NonEmptyOutput.txt"> nul || goto err
 echo test 3 passed
+
+REM  Copy missing file should fail
+%MyProgram% none.txt "%TEMP%\tests\none.txt" > nul && goto err
+echo test 4 passed
 
 REM  If output file is not specified, program must fail
 %MyProgram% none.txt > nul && goto err
-echo test 4 passed
+echo test 5 passed
 
 REM  Copy not should file
 %MyProgram% >nul && goto err
-echo test 5 passed
+echo test 6 passed
 
 REM Тесты завершились успешно
 echo All tests passed successfuly
