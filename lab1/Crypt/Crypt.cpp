5aabe8af0c69087731d6eec1179608574081b444
venera13 <lobanova_venera@mail.ru>
2020-03-15 20:26:33 +0300

    Создала новый проект для программы Сrypt

---
+++ lab1/Crypt/Crypt.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <fstream>
+#include <optional>
+#include <sstream>
+
+using namespace std;
+
+struct Args
+{
+    string cryptType;
+    string inputFileName;
+    string outputFileName;
+	string key;
+};
+
+optional<Args> ParseArg(int argc, char* argv[])
+{
+	if (argc != 5)
+	{
+		cout << "Invalid arguments count\n";
+		cout << "Usage: crypt.exe <crype type> <input file> <output file> <key>\n";
+		return nullopt;
+	}
+	Args args;
+	args.cryptType = argv[1];
+	args.inputFileName = argv[2];
+	args.outputFileName = argv[3];
+	args.key = argv[4];
+	return args;
+}
+
+optional<string> GetValideCrypeType(string cryptType)
+{
+	if(cryptType != "crypt" || cryptType != "decrypt")
+	{
+		cout << "Crypt type not valide\n";
+		return nullopt;
+	}
+	return cryptType;
+}
+
+int main(int argc, char* argv[])
+{
+    auto args = ParseArg(argc, argv);
+	if (!args)
+	{
+		cout << "Invalid arguments count\n";
+		cout << "Usage: crypt.exe <crype type> <input file> <output file> <key>\n";
+		return 1;
+	}
+	args->cryptType = *GetValideCrypeType(args->cryptType);
+	cout << args->cryptType << "\n";
+	return 0;
+}
