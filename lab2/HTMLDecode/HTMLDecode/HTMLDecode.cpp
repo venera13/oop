fdf0ccbf5021885f54527464058da0d236621cb2
Venera Lobanova <venera.lobanova@ispringsolutions.com>
2020-03-26 18:30:30 +0300

    Добавила структуру для функции htmlDecode

--- lab2/HTMLDecode/HTMLDecode/HTMLDecode.cpp
+++ lab2/HTMLDecode/HTMLDecode/HTMLDecode.cpp
@@ -6,6 +6,15 @@
 
 using namespace std;
 
+struct Entities
+{
+	string quotationMark = "&quot;";
+	string singleQuote = "&apos;";
+	string lessThanSign = "&lt;";
+	string greaterThanSign = "&gt;";
+	string ampersand = "&amp;";
+};
+
 string HtmlDecode(string const& html)
 {
 	for_each(html.begin(), html.end(), [](char ch) {
